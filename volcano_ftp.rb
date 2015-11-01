9d37dd92c71d25c77bebbaff5e4fa4693da13501
Olivier El Mekki <olivier@el-mekki.com>
2015-11-01 11:01:13 +0100

    Use attr_reader
    When setting instance variable that will be used mostly for reading, we
    usually put them as `attr_reader` attribute. This allows to access the
    variable without `@`, as if it were a method.
    
    The reason for that is that it's hard to track the value of an instance
    variable. If it's a variable, we expect its value will change. What is
    its value when you write a new method that use it? Is it set at all?
    (this is a real question, because you can access any
    @unexisting_variable, it just returns nil).
    
    This confusion is especially not necessary since you usually just set
    the variable in the class constructor, then keep it unchanged: you just
    want to access a value that come from outside.
    
    Using `attr_reader`, a developer reading your code know for sure the
    value won't change, and can easily find where it's set.

--- volcano_ftp.rb
+++ volcano_ftp.rb
@@ -15,65 +15,65 @@ MAX_PORT = 65534
 
 # Volcano FTP class
 class VolcanoFtp
+  attr_reader :socket, :pids, :cs
+
   def initialize(port)
     # Prepare instance
     @socket = TCPServer.new("127.0.0.1", port)
-    @socket.listen(42)
+    socket.listen(42)
 
     @pids = []
-    @transfert_type = BINARY_MODE
-    @tsocket = nil
     puts "Server ready to listen for clients on port #{port}"
   end
 
   def ftp_syst(args)
-    @cs.write "215 UNIX Type: L8\r\n"
+    cs.write "215 UNIX Type: L8\r\n"
     0
   end
 
   def ftp_noop(args)
-    @cs.write "200 Don't worry my lovely client, I'm here ;)"
+    cs.write "200 Don't worry my lovely client, I'm here ;)"
     0
   end
 
   def ftp_502(*args)
     puts "Command not found"
-    @cs.write "502 Command not implemented\r\n"
+    cs.write "502 Command not implemented\r\n"
     0
   end
 
   def ftp_exit(args)
-    @cs.write "221 Thank you for using Volcano FTP\r\n"
+    cs.write "221 Thank you for using Volcano FTP\r\n"
     -1
   end
 
   def run
     while (42)
-      selectResult = IO.select([@socket], nil, nil, 0.1)
-      if selectResult == nil or selectResult[0].include?(@socket) == false
-        @pids.each do |pid|
+      selectResult = IO.select([socket], nil, nil, 0.1)
+      if selectResult == nil or selectResult[0].include?(socket) == false
+        pids.each do |pid|
           if not Process.waitpid(pid, Process::WNOHANG).nil?
             ####
             # Do stuff with newly terminated processes here
 
             ####
-            @pids.delete(pid)
+            pids.delete(pid)
           end
         end
-        p @pids
+        p pids
       else
-        @cs,  = @socket.accept
-        @pids << Kernel.fork do
-          puts "[#{Process.pid}] Instanciating connection from #{@cs.peeraddr[2]}:#{@cs.peeraddr[1]}"
-          @cs.write "220-\r\n\r\n Welcome to Volcano FTP server !\r\n\r\n220 Connected\r\n"
-          while not (line = @cs.gets).nil?
+        @cs,  = socket.accept
+        pids << Kernel.fork do
+          puts "[#{Process.pid}] Instanciating connection from #{cs.peeraddr[2]}:#{cs.peeraddr[1]}"
+          cs.write "220-\r\n\r\n Welcome to Volcano FTP server !\r\n\r\n220 Connected\r\n"
+          while not (line = cs.gets).nil?
             puts "[#{Process.pid}] Client sent : --#{line}--"
             ####
             # Handle commands here
             ####
           end
           puts "[#{Process.pid}] Killing connection from #{peeraddr[2]}:#{peeraddr[1]}"
-          @cs.close
+          cs.close
           Kernel.exit!
         end
       end
